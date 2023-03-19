41fae35678162c85d4b3d7df33688f6a92ea516c
Ramshri-Mohapatra <124039645+Ramshri-Mohapatra@users.noreply.github.com>
2023-03-19 22:11:56 +0000

    Momentum added
    made the ending of jump smooth

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,11 +16,12 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxis("Horizontal");
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            
         }
 
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
@@ -42,6 +43,7 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+       
     }
 
 
